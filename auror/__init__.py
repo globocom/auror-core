795fe90da6d6ee0a37b9fda2f5b25f8009a893e3
Diogo Munaro <diogo.munaro@corp.globo.com>
2018-11-08 19:56:33 -0200

    Using v1 and v2

--- auror/__init__.py
+++ auror/__init__.py
@@ -1,13 +1,24 @@
 import copy
-from jproperties import Properties
 
 class Project(object):
 
     def __init__(self, folder, *jobtypes):
         self.jobtypes = jobtypes
         self.folder = folder
-        self.properties = Properties()
         self.params = []
+        self.version = 1
+
+    def is_v2(self):
+        self.version = 2
+        return copy.deepcopy(self)
+
+    def is_v1(self):
+        self.version = 1
+        return copy.deepcopy(self)
+
+    def with_params(self, *paramtypes):
+        self.params = paramtypes
+        return copy.deepcopy(self)
 
     def write(self):
         for jobtype in self.jobtypes:
@@ -17,8 +28,3 @@ class Project(object):
         for param in self.params:
             param._add_items()
             param._write(self.folder)
-
-    def with_params(self, *paramtypes):
-        self.params = paramtypes
-
-        return copy.deepcopy(self)
