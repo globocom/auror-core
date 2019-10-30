ec048811c49c5cb14ab3b1bb775b3d9677288db9
Pablo Aguilar <pablo.aguilar@fatec.sp.gov.br>
2019-10-29 21:23:44 -0300

    Adds fallback class when no class was specified in 'get_job_type_class' method

--- auror_core/v2/__init__.py
+++ auror_core/v2/__init__.py
@@ -6,7 +6,4 @@ class JobType:
 
     @staticmethod
     def get_job_type_class(job_type):
-        _job_type = getattr(JobType, job_type.upper(), None)
-        if not _job_type:
-            raise ValueError('Invalid job type: {}'.format(job_type))
-        return _job_type
+        return getattr(JobType, job_type.upper(), Command)
